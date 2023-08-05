98135b180aa401cbe26ddaace5ccdfa848ebb49e
ntvm <134743763+ntvm@users.noreply.github.com>
2023-08-06 00:02:31 +0300

    Settings menu

--- public/scripts/extensions.js
+++ public/scripts/extensions.js
@@ -73,8 +73,8 @@ let extension_settings = {
         fluctuation: 0.1,
         enabled: false,
     },
-    speech_recognition: {},
     Nvkun: {},
+    speech_recognition: {},
 };
 
 let modules = [];
