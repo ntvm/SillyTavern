495cf8e52381a9e0ded8ff4447be9e1a43b13883
Der <nvtm345@gmail.com>
2023-09-01 21:58:12 +0300

    In ST updates attempts

--- server.js
+++ server.js
@@ -1453,7 +1453,16 @@ app.post("/updatestats", jsonParser, function (request, response) {
     return response.sendStatus(200);
 });
 
+/*
+app.post("/getUpdate", jsonParser, function (request, response) {
+const { exec } = require('child_process');
 
+const parentDir = __dirname; 
+const updateBatPath = `${parentDir}/../update.bat`;
+
+exec(`start ${updateBatPath}`);
+});
+*/
 
 app.post("/getbackgrounds", jsonParser, function (request, response) {
     var images = getImages("public/backgrounds");
